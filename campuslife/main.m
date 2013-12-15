06909c23378c3d5bdd6938e7bdc6812cdcfb3ad3
rarmitage <rarmitage@lcmail.lcsc.edu>
2013-12-14 20:26:54 -0800

    changes to main.m

--- campuslife/main.m
+++ campuslife/main.m
@@ -16,3 +16,13 @@ int main(int argc, char * argv[])
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
+
+
+// Maximum number of bytes that a text message may have. The payload data of
+// a push notification is limited to 256 bytes and that includes the JSON
+// overhead and the name of the sender.
+#define MaxMessageLength 190
+#define ServerApiURL @"http://192.168.0.33:44450/api.php"
+
+// Convenience function to show a UIAlertView
+void ShowErrorAlert(NSString* text);
